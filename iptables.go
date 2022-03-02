aacd93a71d5836dd527bcf60386012fa560fd908
mritd <mritd@linux.com>
2022-03-02 12:28:56 +0800

    feat(iptables): remove output uid rules
    remove output uid rules
    
    Signed-off-by: mritd <mritd@linux.com>

--- iptables.go
+++ iptables.go
@@ -61,14 +61,24 @@ func applyIPTables() error {
 
 	/* Forward Network Rules */
 
+	// iptables -t mangle -N TP_CLASH_V4
 	if err = createChain(ip4, tableMangle, chainIP4); err != nil {
 		return err
 	}
 
+	// iptables -t mangle -A TP_CLASH_V4 -d 0.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 127.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 10.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 172.16.0.0/12 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 192.168.0.0/16 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 169.254.0.0/16 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 224.0.0.0/4 -j RETURN
+	// iptables -t mangle -A TP_CLASH_V4 -d 240.0.0.0/4 -j RETURN
 	if err = applyLocalNetwork(ip4, tableMangle, chainIP4); err != nil {
 		return err
 	}
 
+	// iptables -t mangle -A TP_CLASH_V4 -p udp -m udp --dport 53 -j RETURN
 	err = ip4.AppendUnique(tableMangle, chainIP4, "-p", "udp", "--dport", "53", "-j", actionReturn)
 	if err != nil {
 		return fmt.Errorf("failed to append forward dns skip rules: %v", err)
@@ -76,14 +86,30 @@ func applyIPTables() error {
 
 	/* Gateway Network Rules */
 
+	// iptables -t mangle -N TP_CLASH_LOCAL_V4
 	if err = createChain(ip4, tableMangle, chainIP4Local); err != nil {
 		return err
 	}
 
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 0.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 127.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 10.0.0.0/8 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 172.16.0.0/12 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 192.168.0.0/16 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 169.254.0.0/16 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 224.0.0.0/4 -j RETURN
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -d 240.0.0.0/4 -j RETURN
 	if err = applyLocalNetwork(ip4, tableMangle, chainIP4Local); err != nil {
 		return err
 	}
 
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -m owner --uid-owner tpclash -j RETURN
+	err = ip4.AppendUnique(tableMangle, chainIP4Local, "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
+	if err != nil {
+		return fmt.Errorf("failed to append gateway dns skip rules: %v", err)
+	}
+
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -p udp -m udp --dport 53 -j RETURN
 	err = ip4.AppendUnique(tableMangle, chainIP4Local, "-p", "udp", "--dport", "53", "-j", actionReturn)
 	if err != nil {
 		return fmt.Errorf("failed to append gateway dns skip rules: %v", err)
@@ -92,18 +118,24 @@ func applyIPTables() error {
 	/* TProxy Rules */
 
 	logrus.Debug("[iptables] checking tproxy rules...")
+
+	// iptables -t mangle -A TP_CLASH_V4 -p tcp -j TPROXY --on-port 7893 --on-ip 0.0.0.0 --tproxy-mark 0x29a/0xffffffff
 	err = ip4.AppendUnique(tableMangle, chainIP4, "-p", "tcp", "-j", actionTProxy, "--on-port", conf.TProxyPort, "--tproxy-mark", tproxyMark)
 	if err != nil {
 		return fmt.Errorf("failed to append tcp trpoxy rules: %v", err)
 	}
+	// iptables -t mangle -A TP_CLASH_V4 -p udp -j TPROXY --on-port 7893 --on-ip 0.0.0.0 --tproxy-mark 0x29a/0xffffffff
 	err = ip4.AppendUnique(tableMangle, chainIP4, "-p", "udp", "-j", actionTProxy, "--on-port", conf.TProxyPort, "--tproxy-mark", tproxyMark)
 	if err != nil {
 		return fmt.Errorf("failed to append udp trpoxy rules: %v", err)
 	}
+
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -p tcp -j MARK --set-xmark 0x29a/0xffffffff
 	err = ip4.AppendUnique(tableMangle, chainIP4Local, "-p", "tcp", "-j", actionMark, "--set-mark", tproxyMark)
 	if err != nil {
 		return fmt.Errorf("failed to append tcp trpoxy rules: %v", err)
 	}
+	// iptables -t mangle -A TP_CLASH_LOCAL_V4 -p udp -j MARK --set-xmark 0x29a/0xffffffff
 	err = ip4.AppendUnique(tableMangle, chainIP4Local, "-p", "udp", "-j", actionMark, "--set-mark", tproxyMark)
 	if err != nil {
 		return fmt.Errorf("failed to append tcp trpoxy rules: %v", err)
@@ -111,20 +143,25 @@ func applyIPTables() error {
 
 	/* DNS Rules */
 
+	// iptables -t nat -N TP_CLASH_DNS_V4
 	if err = createChain(ip4, tableNat, chainIP4DNS); err != nil {
 		return err
 	}
 	logrus.Debugf("[iptables] checking chain %s/%s rules...", tableNat, chainIP4DNS)
+	// iptables -t nat -A TP_CLASH_DNS_V4 -p udp -m udp --dport 53 -j REDIRECT --to-ports 1053
 	err = ip4.AppendUnique(tableNat, chainIP4DNS, "-p", "udp", "--dport", "53", "-j", actionRedirect, "--to", conf.DNSPort)
 	if err != nil {
 		return fmt.Errorf("failed to append dns rules: %v", err)
 	}
 
+	// iptables -t nat -N TP_CLASH_DNS_LOCAL_V4
 	if err = createChain(ip4, tableNat, chainIP4DNSLocal); err != nil {
 		return err
 	}
 	logrus.Debugf("[iptables] checking chain %s/%s rules...", tableNat, chainIP4DNSLocal)
+	// iptables -t nat -A TP_CLASH_DNS_LOCAL_V4 -m owner --uid-owner tpclash -j RETURN
 	err = ip4.AppendUnique(tableNat, chainIP4DNSLocal, "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
+	// iptables -t nat -A TP_CLASH_DNS_LOCAL_V4 -p udp -m udp --dport 53 -j REDIRECT --to-ports 1053
 	if err != nil {
 		return fmt.Errorf("failed to append dns rules: %v", err)
 	}
@@ -133,26 +170,17 @@ func applyIPTables() error {
 		return fmt.Errorf("failed to append dns rules: %v", err)
 	}
 
-	/* TPClash Output Rules */
-
-	logrus.Debug("[iptables] checking tpclash output rules...")
-	err = ip4.AppendUnique(tableMangle, chainOutput, "-p", "tcp", "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
-	if err != nil {
-		return fmt.Errorf("failed to append dns rules: %v", err)
-	}
-	err = ip4.AppendUnique(tableMangle, chainOutput, "-p", "udp", "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
-	if err != nil {
-		return fmt.Errorf("failed to append dns rules: %v", err)
-	}
-
 	/* Fix ICMP */
 
 	logrus.Debug("[iptables] checking icmp fake rules...")
+
+	// iptables -t nat -A PREROUTING -d 198.18.0.0/16 -p icmp -j DNAT --to-destination 127.0.0.1
 	err = ip4.AppendUnique(tableNat, chainPreRouting, "-p", "icmp", "-d", conf.FakeIPRange, "-j", actionDNat, "--to-destination", "127.0.0.1")
 	if err != nil {
 		return fmt.Errorf("failed to append icmp fake rules: %v", err)
 	}
 
+	// iptables -t nat -A OUTPUT -d 198.18.0.0/16 -p icmp -j DNAT --to-destination 127.0.0.1
 	err = ip4.AppendUnique(tableNat, chainOutput, "-p", "icmp", "-d", conf.FakeIPRange, "-j", actionDNat, "--to-destination", "127.0.0.1")
 	if err != nil {
 		return fmt.Errorf("failed to append icmp fake rules: %v", err)
@@ -178,18 +206,23 @@ func applyIPTables() error {
 		return fmt.Errorf("failed to delete rules: %s/%s -> %s, error: %v", tableNat, chainOutput, chainIP4DNSLocal, err)
 	}
 
+	// iptables -t mangle -A PREROUTING -j TP_CLASH_V4
 	err = ip4.AppendUnique(tableMangle, chainPreRouting, "-j", chainIP4)
 	if err != nil {
 		return fmt.Errorf("failed to apply rules: %s/%s -> %s, error: %v", tableMangle, chainPreRouting, chainIP4, err)
 	}
-	err = ip4.AppendUnique(tableMangle, chainOutput, "-j", chainIP4Local)
-	if err != nil {
-		return fmt.Errorf("failed to apply rules: %s/%s -> %s, error: %v", tableMangle, chainOutput, chainIP4Local, err)
-	}
+	// iptables -t nat -A PREROUTING -j TP_CLASH_DNS_V4
 	err = ip4.AppendUnique(tableNat, chainPreRouting, "-j", chainIP4DNS)
 	if err != nil {
 		return fmt.Errorf("failed to apply rules: %s/%s -> %s, error: %v", tableNat, chainPreRouting, chainIP4DNS, err)
 	}
+
+	// iptables -t mangle -A OUTPUT -j TP_CLASH_LOCAL_V4
+	err = ip4.AppendUnique(tableMangle, chainOutput, "-j", chainIP4Local)
+	if err != nil {
+		return fmt.Errorf("failed to apply rules: %s/%s -> %s, error: %v", tableMangle, chainOutput, chainIP4Local, err)
+	}
+	// iptables -t nat -A OUTPUT -j TP_CLASH_DNS_LOCAL_V4
 	err = ip4.AppendUnique(tableNat, chainOutput, "-j", chainIP4DNSLocal)
 	if err != nil {
 		return fmt.Errorf("failed to apply rules: %s/%s -> %s, error: %v", tableNat, chainOutput, chainIP4DNSLocal, err)
@@ -286,17 +319,5 @@ func cleanIPTables() {
 	err = ip4.DeleteIfExists(tableNat, chainOutput, "-p", "icmp", "-d", conf.FakeIPRange, "-j", actionDNat, "--to-destination", "127.0.0.1")
 	if err != nil {
 		logrus.Errorf("failed to delete icmp fake rules: %v", err)
-	}
-
-	if checkUser() {
-		logrus.Debug("[iptables] clean tpclash output...")
-		err = ip4.DeleteIfExists(tableMangle, chainOutput, "-p", "tcp", "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
-		if err != nil {
-			logrus.Errorf("failed to delete tpclash output rules: %v", err)
-		}
-		err = ip4.DeleteIfExists(tableMangle, chainOutput, "-p", "udp", "-m", "owner", "--uid-owner", clashUser, "-j", actionReturn)
-		if err != nil {
-			logrus.Errorf("failed to delete tpclash output rules: %v", err)
-		}
 	}
 }
