d6c55544fd16acad51999a3245f08fcda1b01cdc
kovacs <mritd@linux.com>
2023-04-24 22:22:15 +0800

    feat(config): add log prefix for config check
    add log prefix for config check
    
    Signed-off-by: kovacs <mritd@linux.com>

--- config.go
+++ config.go
@@ -42,49 +42,49 @@ func ParseClashConf() (*ClashConf, error) {
 
 	// common check
 	if strings.ToLower(enhancedMode) != "fake-ip" {
-		return nil, fmt.Errorf("only support fake-ip dns mode(dns.enhanced-mode)")
+		return nil, fmt.Errorf("[conf] only support fake-ip dns mode(dns.enhanced-mode)")
 	}
 
 	dnsHost, dnsPort, err := net.SplitHostPort(dnsListen)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse clash dns listen config(dns.listen): %v", err)
+		return nil, fmt.Errorf("[conf] failed to parse clash dns listen config(dns.listen): %v", err)
 	}
 
 	dport, err := strconv.Atoi(dnsPort)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse clash dns listen config(dns.listen): %v", err)
+		return nil, fmt.Errorf("[conf] failed to parse clash dns listen config(dns.listen): %v", err)
 	}
 	if dport < 1 {
-		return nil, fmt.Errorf("dns port in clash config is missing(dns.listen)")
+		return nil, fmt.Errorf("[conf] dns port in clash config is missing(dns.listen)")
 	}
 
 	dhost := net.ParseIP(dnsHost)
 	if dhost == nil {
-		return nil, fmt.Errorf("dns listening address parse failed(dns.listen): is not a valid IP address")
+		return nil, fmt.Errorf("[conf] dns listening address parse failed(dns.listen): is not a valid IP address")
 	}
 
 	if interfaceName == "" {
-		return nil, fmt.Errorf("failed to parse clash interface name(interface-name): interface-name must be set")
+		return nil, fmt.Errorf("[conf] failed to parse clash interface name(interface-name): interface-name must be set")
 	}
 
 	if fakeIPRange == "" {
-		return nil, fmt.Errorf("failed to parse clash fake ip range name(dns.fake-ip-range): fake-ip-range must be set")
+		return nil, fmt.Errorf("[conf] failed to parse clash fake ip range name(dns.fake-ip-range): fake-ip-range must be set")
 	}
 
 	if !tunEnabled {
-		return nil, fmt.Errorf("tun must be enabled in tun mode(tun.enable)")
+		return nil, fmt.Errorf("[conf] tun must be enabled in tun mode(tun.enable)")
 	}
 
 	if !tunAutoRoute && tunEBPF == nil {
-		return nil, fmt.Errorf("must be enabled auto-route or ebpf in tun mode(tun.auto-route/ebpf.redirect-to-tun)")
+		return nil, fmt.Errorf("[conf] must be enabled auto-route or ebpf in tun mode(tun.auto-route/ebpf.redirect-to-tun)")
 	}
 
 	if tunAutoRoute && tunEBPF != nil {
-		return nil, fmt.Errorf("cannot enable auto-route and ebpf at the same time(tun.auto-route/ebpf.redirect-to-tun)")
+		return nil, fmt.Errorf("[conf] cannot enable auto-route and ebpf at the same time(tun.auto-route/ebpf.redirect-to-tun)")
 	}
 
 	if tunEBPF != nil && routingMark == 0 {
-		return nil, fmt.Errorf("ebpf needs to set routing-mark(routing-mark)")
+		return nil, fmt.Errorf("[conf] ebpf needs to set routing-mark(routing-mark)")
 	}
 
 	if metaIPtables {
