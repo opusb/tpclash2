ac48e29dc059e8d81c5e47c06ab7be66352402fa
kovacs <mritd@linux.com>
2023-07-27 18:08:09 +0800

    feat(cmd): add un/install cmd
    add un/install cmd
    
    Signed-off-by: kovacs <mritd@linux.com>

--- constant.go
+++ constant.go
@@ -24,3 +24,41 @@ const logo = `
    ██║   ██║     ╚██████╗███████╗██║  ██║███████║██║  ██║
    ╚═╝   ╚═╝      ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
 `
+
+const systmedTpl = `[Unit]
+Description=Transparent proxy tool for Clash
+After=network.target
+
+[Service]
+Type=simple
+User=root
+Restart=on-failure
+ExecStart=/usr/local/bin/tpclash%s
+
+RestartSec=10s
+TimeoutStopSec=30s
+
+[Install]
+WantedBy=multi-user.target
+`
+
+const (
+	installDir = "/usr/local/bin"
+	systemdDir = "/etc/systemd/system"
+)
+
+const installedMessage = logo + `  👌 TPClash 安装完成, 您可以使用以下命令启动:
+    - 启动服务: systemctl start tpclash
+    - 停止服务: systemctl stop tpclash
+    - 开启自启动: systemctl enable tpclash
+    - 关闭自启动: systemctl disable tpclash
+	- 查看日志: journalctl -fu tpclash
+`
+
+const uninstallMessage = `  
+  ⚠️ 在卸载前请务必先停止 TPClash
+  ⚠️ 如果尚未停止请按 Ctrl+c 终止卸载
+  ⚠️ 本卸载程序将会在 30s 后继续执行卸载
+`
+
+const uninstalledMessage = logo + `  👌 TPClash 已卸载`
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -175,10 +176,101 @@ var decCmd = &cobra.Command{
 	},
 }
 
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Install TPClash",
+	Run: func(cmd *cobra.Command, args []string) {
+		exePath, err := os.Executable()
+		if err != nil {
+			logrus.Fatalf("[install] unable to get executable file path: %v", err)
+		}
+
+		err = os.MkdirAll(installDir, 0755)
+		if err != nil {
+			logrus.Fatalf("[install] failed to create directory: %v", err)
+		}
+
+		src, err := os.Open(exePath)
+		if err != nil {
+			logrus.Fatalf("[install] failed to open tpclash file: %v", err)
+		}
+		defer func() { _ = src.Close() }()
+
+		dst, err := os.OpenFile(filepath.Join(installDir, "tpclash"), os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0755)
+		if err != nil {
+			logrus.Fatalf("[install] failed to create executable file: %v", err)
+		}
+		defer func() { _ = dst.Close() }()
+
+		_, err = io.Copy(dst, src)
+		if err != nil {
+			logrus.Fatalf("[install] failed to copy executable file: %v", err)
+		}
+
+		opts := ""
+		if conf.Debug {
+			opts += "--debug"
+		}
+		if conf.ClashHome != "" {
+			opts += fmt.Sprintf(" %s %s", "--home", conf.ClashHome)
+		}
+		if conf.ClashConfig != "" {
+			opts += fmt.Sprintf(" %s %s", "--config", conf.ClashConfig)
+		}
+		if conf.ClashUI != "" {
+			opts += fmt.Sprintf(" %s %s", "--ui", conf.ClashUI)
+		}
+		if conf.CheckInterval > 0 {
+			opts += fmt.Sprintf(" %s %s", "--check-interval", conf.CheckInterval.String())
+		}
+		if len(conf.HttpHeader) > 0 {
+			for _, h := range conf.HttpHeader {
+				opts += fmt.Sprintf(" %s %s", "--http-header", h)
+			}
+		}
+		if conf.ConfigEncPassword != "" {
+			opts += fmt.Sprintf(" %s %s", "--config-password", conf.ConfigEncPassword)
+		}
+		if conf.DisableExtract {
+			opts += "--disable-extract"
+		}
+
+		err = os.WriteFile(filepath.Join(systemdDir, "tpclash.service"), []byte(fmt.Sprintf(systmedTpl, opts)), 0644)
+		if err != nil {
+			logrus.Fatalf("[install] failed to create systemd service: %v", err)
+		}
+
+		fmt.Print(installedMessage)
+	},
+}
+
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "Uninstall TPClash",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print(uninstallMessage)
+		time.Sleep(30 * time.Second)
+
+		logrus.Warnf("[uninstall] remove --> %s", filepath.Join(installDir, "tpclash"))
+		err := os.RemoveAll(filepath.Join(installDir, "tpclash"))
+		if err != nil {
+			logrus.Fatalf("[uninstall] failed to remove executable file: %v", err)
+		}
+
+		logrus.Warnf("[uninstall] remove --> %s", filepath.Join(systemdDir, "tpclash.service"))
+		err = os.RemoveAll(filepath.Join(systemdDir, "tpclash.service"))
+		if err != nil {
+			logrus.Fatalf("[uninstall] failed to remove systemd service: %v", err)
+		}
+
+		fmt.Print(uninstalledMessage)
+	},
+}
+
 func init() {
 	cobra.EnableCommandSorting = false
 
-	rootCmd.AddCommand(encCmd, decCmd)
+	rootCmd.AddCommand(encCmd, decCmd, installCmd, uninstallCmd)
 
 	rootCmd.PersistentFlags().BoolVar(&conf.Debug, "debug", false, "enable debug log")
 	rootCmd.PersistentFlags().BoolVar(&conf.Test, "test", false, "enable test mode, tpclash will automatically exit after 5 minutes")
